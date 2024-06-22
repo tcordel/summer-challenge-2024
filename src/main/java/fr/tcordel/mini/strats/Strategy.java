44b54d32ae52dcc15c8294d291e71f7c1fbc4e19
Thibault Cordel <tcordel@enovacom.fr>
2024-06-22 14:20:52 +0200

    fix: seems better without roller

--- src/main/java/fr/tcordel/mini/strats/Strategy.java
+++ src/main/java/fr/tcordel/mini/strats/Strategy.java
@@ -38,7 +38,7 @@ public interface Strategy {
 		Strategy strategy = switch (gameId) {
 			case 0 -> hurdleRaceStrategy;
 			case 1 -> new ArcheryStrategy(gpu, reg0, reg1, reg2, reg3, reg4, reg5, reg6);
-			case 2 -> new RollerSpeedSkatingStrategy(gpu, reg0, reg1, reg2, reg3, reg4, reg5, reg6);
+			// case 2 -> new RollerSpeedSkatingStrategy(gpu, reg0, reg1, reg2, reg3, reg4, reg5, reg6);
 			case 3 -> new DivingStrategy(gpu, reg0, reg1, reg2, reg3, reg4, reg5, reg6);
 			default -> new GameOverStrategy();
 		};
